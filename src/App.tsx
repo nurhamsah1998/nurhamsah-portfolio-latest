c95c22c239d2bbc7d7eb0ffb56c5a2c050523dd6
Nurhamsah <nur.hamsah.cash@gmail.com>
2023-03-23 13:59:13 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Screen/Header";
+import Navbar from "./Screen/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Screen/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className=" bg-red-500 h-[100vh] w-full">
+      <p className=" text-3xl">asdsad</p>
+    </div>
+  );
+}
+
+export default Header;
